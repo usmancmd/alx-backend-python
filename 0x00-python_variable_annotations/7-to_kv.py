0f71f83c2ee10559c0b94e9c21ecc5afbde2a29a
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-05 13:24:20 +0100

    Add solution to task-7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Type-annotated function to_kv that
 takes a string k and an int OR float v as
@@ -8,4 +9,5 @@ from typing import Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> tuple[Union[str, float]]:
+    """Returns a tuple"""
     return (k, v ** 2)
