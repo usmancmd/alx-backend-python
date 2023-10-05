20b3bb0537157affcbdf692f7f6fba31bd071983
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-05 11:53:35 +0100

    Add solution to task-101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,10 +4,13 @@ add type annotations to the function
 """
 
 from typing import Mapping, Union, Any, TypeVar
+T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key:Any, default: Union[TypeVar('T'), None] = None)\
-        -> Union[Any, TypeVar('T')]:
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[TypeVar('T'), None] = None)\
+                     -> Union[Any, TypeVar('T')]:
     if key in dct:
         return dct[key]
     else:
