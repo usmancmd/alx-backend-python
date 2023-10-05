d0341b9afaaaf9264d4e3044983d9ba64b8c5c14
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-05 13:12:47 +0100

    Add solution to task-3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Type-annotated function to_str that
 takes a float n as argument and returns
@@ -6,4 +7,5 @@ the string representation of the float
 
 
 def to_str(n: float) -> str:
+    """Accepts float and returns string"""
     return str(n)
