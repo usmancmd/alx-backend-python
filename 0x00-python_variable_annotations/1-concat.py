a4031fab3974d044398952d6491a0c63c47e569c
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-05 07:36:00 +0100

    Add solution to task-2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+"""
+Type-annotated function concat that
+takes a string str1 and a string str2 as
+arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
