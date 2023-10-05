9baabcf80c6a0f72d12257a955ee00200ee5845d
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-05 13:35:00 +0100

    Add solution to task-100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Augment the following code with the correct duck-typed annotations
 """
@@ -7,6 +8,7 @@ from typing import Sequence, Any, Union
 
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Returns correct duck-typed annotations"""
     if lst:
         return lst[0]
     else:
