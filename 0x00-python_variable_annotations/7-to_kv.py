0ecff6697221b042a617ef61fc226f963be2270d
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-05 13:28:22 +0100

    Add solution to task-7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,9 +5,9 @@ takes a string k and an int OR float v as
 arguments and returns a tuple
 """
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[Union[str, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple"""
     return (k, v ** 2)
