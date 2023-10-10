ec833109b52fbedf6472e809105fc2e0799e9131
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-10 12:03:12 +0100

    Add solution to task-4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,12 +7,11 @@ import asyncio
 from typing import List
 from heapq import heappush, heappop
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawn wait_random n times"""
+    """Spawn task_wait_random n times"""
     heap = []
     delays = []
 
