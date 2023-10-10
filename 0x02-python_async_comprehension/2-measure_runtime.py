6cdbf63175fd6c17066ebc1c8fec5d5b30001afb
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-10 17:37:56 +0100

    Add solution to task-2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,12 +5,11 @@ execute async_comprehension four times
 in parallel using asyncio.gather
 """
 import asyncio
-from typing import List
 from time import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     start_time = time()
     await asyncio.gather(
             async_comprehension(),
