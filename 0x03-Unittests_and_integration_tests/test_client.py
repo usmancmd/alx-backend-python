975d330b842a4a4f04a31aa8e7c022721132b86f
Usman Adepoju <adepojuusman465@gmail.com>
2023-10-29 17:59:59 +0100

    Add TestGithubOrgClient class

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""GithubOrgClient Test class"""
+
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """TestGithubOrgClient class"""
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"})
+    ])
+    @patch('client.get_json')
+    def test_org(self, test_org, expected_result, mock_get_json):
+        """test_org method"""
+        mock_get_json.return_value = expected_result
+
+        test_instance = GithubOrgClient(test_org)
+
+        result = test_instance.org
+
+        mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{test_org}')
+
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
